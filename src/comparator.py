a95d45d88de56cec8ea75090e22e35f3163a8501
Jakub Tolsciuk <jakubtolsciuk@gmail.com>
2022-12-18 21:23:34 +0100

    Code refactor

--- src/comparator.py
+++ src/comparator.py
@@ -15,14 +15,14 @@ from typing import List
 from utils import Logger
 
 
-class ClassifiersComparator:
+class Comparator:
     N_SPLITS = 5
     N_REPEATS = 2
     N_COMPONENTS = 2    # number of features left after PCA operation
     ALFA = 0.05
 
-    def __init__(self, classifiers, dataset: ndarray, logger: Logger, dataset_name: str) -> None:
-        self._classifiers = classifiers
+    def __init__(self, classifier, dataset: ndarray, logger: Logger, dataset_name: str) -> None:
+        self._classifier = classifier
         self._dataset = dataset
         self._logger = logger
         self._dataset_name = dataset_name
@@ -36,50 +36,43 @@ class ClassifiersComparator:
 
     def compare(self, oversampling, reverse_pca: bool) -> None:
         self._logger.write(f"Settings[ reverse_pca: {reverse_pca}, oversampling: {oversampling.__class__.__name__}]")
-        acc_score, df_acc = self._calculate_accuracy(oversampling, reverse_pca)
+        acc_score, acc_score_mean = self._calculate_accuracy(oversampling, reverse_pca)
         # df_better = self._do_statystical_analysis(acc_score)
-        return acc_score, df_acc # df_acc, df_better
+        return acc_score, acc_score_mean # df_acc, df_better
 
     @ignore_warnings(category=ConvergenceWarning)
     def _calculate_accuracy(self, oversampling, reverse_pca) -> ndarray:
         X = self._dataset[:, :-1]
         Y = self._dataset[:, -1]
-        scores = np.zeros((self._classifiers_num, self.N_SPLITS*self.N_REPEATS))
+        scores = np.zeros(self.N_SPLITS*self.N_REPEATS)
 
         for fold_id, (train, test) in enumerate(self._rskf.split(X, Y)):
-            for clf_id, clf_name in enumerate(self._classifiers):
-                clf = clone(self._classifiers[clf_name])
+            clf = clone(self._classifier)
 
-                x_train = self._pca.fit_transform(X[train])
-                x_test = X[test] if reverse_pca else self._pca.transform(X[test])
+            x_train = self._pca.fit_transform(X[train])
+            x_test = X[test] if reverse_pca else self._pca.transform(X[test])
 
-                # * oversampling - SMOTE/ROS
-                x_train, y_train = oversampling.fit_resample(x_train, Y[train])
-                if reverse_pca:
-                    x_train = self._pca.inverse_transform(x_train)
+            # * oversampling - SMOTE/ROS
+            x_train, y_train = oversampling.fit_resample(x_train, Y[train])
+            if reverse_pca:
+                x_train = self._pca.inverse_transform(x_train)
 
-                clf.fit(x_train, y_train)
-                y_pred = clf.predict(x_test)
-                scores[clf_id, fold_id] = balanced_accuracy_score(Y[test], y_pred)
+            clf.fit(x_train, y_train)
+            y_pred = clf.predict(x_test)
+            scores[fold_id] = balanced_accuracy_score(Y[test], y_pred)
 
-        mean = np.mean(scores, axis=1)
-        std = np.std(scores, axis=1)
-        for clf_id, clf_name in enumerate(self._classifiers):
-            self._logger.write(f"{clf_name}, {mean[clf_id]}, {std[clf_id]}")
-
-        df = pd.DataFrame(data = [{
-            'dataset': self._dataset_name,
-            'MLP': round(mean[0], 2),
-            'CART': round(mean[1], 2),
-            'KNN': round(mean[2], 2)
-        }])
+        mean = np.mean(scores)
+        std = np.std(scores)
 
         acc_score = np.copy(scores)
         self._logger.write("\n Balanced acc score values:")
         self._logger.write(str(acc_score))
-        return acc_score, df
+        return acc_score, mean
 
     def do_statystical_analysis(self, all_acc_score: List[ndarray]) -> None:
+        print("--------ALL ACC score-----------")
+        print(all_acc_score)
+
         # * init tables
         shape = (4, 4)
         t_statistic = np.zeros(shape)
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from sklearn import tree
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 
-from classifires_comparator import ClassifiersComparator
+from comparator import Comparator
 from loaders import ConfigLoader, KeelDatasetLoader
 from utils import Logger
 import pandas as pd
@@ -13,9 +13,9 @@ CLASSIFIRES = {
     'MLP': MLPClassifier(
         hidden_layer_sizes = (30, 30, 30), max_iter=400,
         random_state = 75, solver = 'sgd'
-    )#,
-    # 'CART': tree.DecisionTreeClassifier(),
-    # 'KNN': KNeighborsClassifier(n_neighbors=3)
+    ),
+    'CART': tree.DecisionTreeClassifier(),
+    'KNN': KNeighborsClassifier(n_neighbors=3)
 }
 
 def main() -> None:
@@ -31,32 +31,43 @@ def main() -> None:
     df_acc = pd.DataFrame()
     df_better = pd.DataFrame()
 
-    for dateset_name in dataset_names:
-        # for classifire
-        logger.write(dateset_name)
-        dataset = dataset_loader.load(dateset_name, convert_to_ndarray=True)
+    for classifire_name in CLASSIFIRES:
+        for dateset_name in dataset_names:
+            logger.write(dateset_name)
+            dataset = dataset_loader.load(dateset_name, convert_to_ndarray=True)
 
-        comparator = ClassifiersComparator(classifiers=CLASSIFIRES, dataset=dataset, logger=logger, dataset_name=dateset_name)
-        all_acc_score = []
-        acc_score, df_acc = comparator.compare(oversampling=smote, reverse_pca=True)
-        all_acc_score.append(acc_score)
-        acc_score, df_acc = comparator.compare(oversampling=smote, reverse_pca=False)
-        all_acc_score.append(acc_score)
-        acc_score, df_acc = comparator.compare(oversampling=ros, reverse_pca=True)
-        all_acc_score.append(acc_score)
-        acc_score, df_acc = comparator.compare(oversampling=ros, reverse_pca=False)
-        all_acc_score.append(acc_score)
+            comparator = Comparator(classifier=CLASSIFIRES[classifire_name], dataset=dataset, logger=logger, dataset_name=dateset_name)
 
-        comparator.do_statystical_analysis(all_acc_score)
+            all_acc_score = []
 
-        # df_acc = pd.concat([output_df_acc, df_acc], ignore_index=True)
-        # df_better = pd.concat([output_df_better, df_better], ignore_index=True)
-        # comparator.compare(oversampling=smote, reverse_pca=False)
-        # comparator.compare(oversampling=ros, reverse_pca=True)
-        # comparator.compare(oversampling=ros, reverse_pca=False)
+            acc_score, acc_score_mean_1 = comparator.compare(oversampling=smote, reverse_pca=True)
+            all_acc_score.append(acc_score)
 
-    df_acc.to_csv('./output/results_acc.csv')
-    df_better.to_csv('./output/results_better.csv')
+            acc_score, acc_score_mean_2 = comparator.compare(oversampling=smote, reverse_pca=False)
+            all_acc_score.append(acc_score)
+
+            acc_score, acc_score_mean_3 = comparator.compare(oversampling=ros, reverse_pca=True)
+            all_acc_score.append(acc_score)
+
+            acc_score, acc_score_mean_4 = comparator.compare(oversampling=ros, reverse_pca=False)
+            all_acc_score.append(acc_score)
+
+            output_df_better = comparator.do_statystical_analysis(all_acc_score)
+            df_better = pd.concat([output_df_better, df_better], ignore_index=True)
+            output_df_acc = pd.DataFrame(data = [{
+                'dataset': dateset_name,
+                "SMOTE-REVERSE": acc_score_mean_1,
+                "SMOTE-NO-REVERSE": acc_score_mean_2,
+                "ROS-REVERSE": acc_score_mean_3,
+                "ROS-NO-REVERSE": acc_score_mean_4,
+            }])
+            df_acc = pd.concat([output_df_acc, df_acc], ignore_index=True)
+
+        df_acc.to_csv(f'./output/results_acc_{classifire_name}.csv')
+        df_acc = pd.DataFrame()
+        df_better.to_csv(f'./output/results_better_{classifire_name}.csv')
+        df_better = pd.DataFrame()
+
 
 if __name__ == "__main__":
     main()
