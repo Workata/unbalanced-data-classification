1778d951e8e771a92d218dfad36bca9cb413e08b
Jakub Tolsciuk <jakubtolsciuk@gmail.com>
2022-12-18 20:07:16 +0100

    Add logs to df

--- src/classifires_comparator.py
+++ src/classifires_comparator.py
@@ -9,6 +9,7 @@ from sklearn.metrics import \
     balanced_accuracy_score  # we can also check accuracy_score
 from sklearn.model_selection import RepeatedStratifiedKFold
 from tabulate import tabulate
+import pandas as pd
 
 from utils import Logger
 
@@ -19,10 +20,11 @@ class ClassifiersComparator:
     N_COMPONENTS = 2    # number of features left after PCA operation
     ALFA = 0.05
 
-    def __init__(self, classifiers, dataset: ndarray, logger: Logger) -> None:
+    def __init__(self, classifiers, dataset: ndarray, logger: Logger, dataset_name: str) -> None:
         self._classifiers = classifiers
         self._dataset = dataset
         self._logger = logger
+        self._dataset_name = dataset_name
         self._rskf = RepeatedStratifiedKFold(n_splits=self.N_SPLITS, n_repeats=self.N_REPEATS, random_state=42)
         self._pca = PCA(n_components=self.N_COMPONENTS)
 
@@ -33,8 +35,9 @@ class ClassifiersComparator:
 
     def compare(self, oversampling, reverse_pca: bool) -> None:
         self._logger.write(f"Settings[ reverse_pca: {reverse_pca}, oversampling: {oversampling.__class__.__name__}]")
-        acc_score = self._calculate_accuracy(oversampling, reverse_pca)
-        self._do_statystical_analysis(acc_score)
+        acc_score, df_acc = self._calculate_accuracy(oversampling, reverse_pca)
+        df_better = self._do_statystical_analysis(acc_score)
+        return df_acc, df_better
 
     @ignore_warnings(category=ConvergenceWarning)
     def _calculate_accuracy(self, oversampling, reverse_pca) -> ndarray:
@@ -63,10 +66,17 @@ class ClassifiersComparator:
         for clf_id, clf_name in enumerate(self._classifiers):
             self._logger.write(f"{clf_name}, {mean[clf_id]}, {std[clf_id]}")
 
+        df = pd.DataFrame(data = [{
+            'dataset': self._dataset_name,
+            'MLP': round(mean[0], 2),
+            'CART': round(mean[1], 2),
+            'KNN': round(mean[2], 2)
+        }])
+
         acc_score = np.copy(scores)
         self._logger.write("\n Balanced acc score values:")
         self._logger.write(str(acc_score))
-        return acc_score
+        return acc_score, df
 
     def _do_statystical_analysis(self, acc_score: ndarray) -> None:
         # * init tables
@@ -99,6 +109,17 @@ class ClassifiersComparator:
         self._logger.write(f"Statistical significance (alpha = 0.05):\n {significance_table}")
 
         stat_better = significance * advantage
+        print(stat_better.shape)
+        print(stat_better)
         stat_better_table = tabulate(np.concatenate((names_column, stat_better), axis=1), headers)
+
+        # check vs advantage
+        df = pd.DataFrame(data = [{
+            'dataset': self._dataset_name,
+            'MLP': np.array2string(np.where(stat_better[0][:] == 1)[0]),
+            'CART': np.array2string(np.where(stat_better[1][:] == 1)[0]),
+            'KNN': np.array2string(np.where(stat_better[2][:] == 1)[0])
+        }])
         self._logger.write(f"Statistically significantly better:\n {stat_better_table}")
         self._logger.write("\n----------------------------------------\n")
+        return df
--- src/main.py
+++ src/main.py
@@ -6,10 +6,11 @@ from sklearn.neural_network import MLPClassifier
 from classifires_comparator import ClassifiersComparator
 from loaders import ConfigLoader, KeelDatasetLoader
 from utils import Logger
+import pandas as pd
 
 CONFIG_FILE_PATH = './config.yaml'
 CLASSIFIRES = {
-    'MLP_A': MLPClassifier(
+    'MLP': MLPClassifier(
         hidden_layer_sizes = (30, 30, 30), max_iter=400,
         random_state = 75, solver = 'sgd'
     ),
@@ -25,17 +26,25 @@ def main() -> None:
     ros = RandomOverSampler(sampling_strategy='auto')
 
     dataset_names = config.get('datasets', [])
+    logger = Logger(log_file_name="output.txt")
+
+    df_acc = pd.DataFrame()
+    df_better = pd.DataFrame()
+
     for dateset_name in dataset_names:
-        logger = Logger(log_file_name=dateset_name.replace(".dat", ".log"))
         logger.write(dateset_name)
         dataset = dataset_loader.load(dateset_name, convert_to_ndarray=True)
 
-        comparator = ClassifiersComparator(classifiers=CLASSIFIRES, dataset=dataset, logger=logger)
-        comparator.compare(oversampling=smote, reverse_pca=True)
-        comparator.compare(oversampling=smote, reverse_pca=False)
-        comparator.compare(oversampling=ros, reverse_pca=True)
-        comparator.compare(oversampling=ros, reverse_pca=False)
+        comparator = ClassifiersComparator(classifiers=CLASSIFIRES, dataset=dataset, logger=logger, dataset_name=dateset_name)
+        output_df_acc, output_df_better = comparator.compare(oversampling=smote, reverse_pca=True)
+        df_acc = pd.concat([output_df_acc, df_acc], ignore_index=True)
+        df_better = pd.concat([output_df_better, df_better], ignore_index=True)
+        # comparator.compare(oversampling=smote, reverse_pca=False)
+        # comparator.compare(oversampling=ros, reverse_pca=True)
+        # comparator.compare(oversampling=ros, reverse_pca=False)
 
+    df_acc.to_csv('./output/results_acc.csv')
+    df_better.to_csv('./output/results_better.csv')
 
 if __name__ == "__main__":
     main()
