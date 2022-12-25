bdc74807ee2d25e8fdd69cbe5d8efa1fe91ad4e2
workata <jakubtolsciuk@gmail.com>
2022-12-25 18:28:01 +0100

    Add single df to latex

--- src/comparator.py
+++ src/comparator.py
@@ -1,18 +1,27 @@
+from enum import Enum
+from typing import List
+
 import numpy as np
+import pandas as pd
 from numpy import ndarray
 from scipy.stats import ttest_rel
 from sklearn import clone
 from sklearn.decomposition import PCA
-from sklearn.utils._testing import ignore_warnings
 from sklearn.exceptions import ConvergenceWarning
 from sklearn.metrics import \
     balanced_accuracy_score  # we can also check accuracy_score
 from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.utils._testing import ignore_warnings
 from tabulate import tabulate
-import pandas as pd
-from typing import List
 
+from models import Dataset
 from utils import Logger
+
+
+class PCAMode(Enum):
+    WITHOUT_PCA = 1
+    WITH_PCA = 2
+    WITH_REVERSE_PCA = 3
 
 
 class Comparator:
@@ -21,30 +30,37 @@ class Comparator:
     N_COMPONENTS = 2    # number of features left after PCA operation
     ALFA = 0.05
 
-    def __init__(self, classifier, dataset: ndarray, logger: Logger, dataset_name: str) -> None:
+    def __init__(self, classifier, dataset: Dataset, logger: Logger) -> None:
         self._classifier = classifier
         self._dataset = dataset
         self._logger = logger
-        self._dataset_name = dataset_name
         self._rskf = RepeatedStratifiedKFold(n_splits=self.N_SPLITS, n_repeats=self.N_REPEATS, random_state=42)
         self._pca = PCA(n_components=self.N_COMPONENTS)
 
     @ignore_warnings(category=ConvergenceWarning)
-    def calculate_accuracy(self, oversampling, reverse_pca) -> ndarray:
-        self._logger.write(f"Settings[ reverse_pca: {reverse_pca}, oversampling: {oversampling.__class__.__name__}]")
-        X = self._dataset[:, :-1]
-        Y = self._dataset[:, -1]
+    def calculate_accuracy(self, oversampling, pca_mode: PCAMode) -> ndarray:
+        self._logger.write(f"Settings[ PCA mode: {pca_mode.name}, oversampling: {oversampling.__class__.__name__}]")
+        df = self._dataset.data
+        X = df[:, :-1]
+        Y = df[:, -1]
         scores = np.zeros(self.N_SPLITS*self.N_REPEATS)
 
         for fold_id, (train, test) in enumerate(self._rskf.split(X, Y)):
             clf = clone(self._classifier)
 
-            x_train = self._pca.fit_transform(X[train])
-            x_test = X[test] if reverse_pca else self._pca.transform(X[test])
+            if pca_mode == PCAMode.WITHOUT_PCA:
+                x_train = X[train]
+            else:
+                x_train = self._pca.fit_transform(X[train])
+
+            if pca_mode == PCAMode.WITH_PCA:
+                x_test = self._pca.transform(X[test])
+            else:   # without pca or reverse pca
+                x_test = X[test]
 
             # * oversampling - SMOTE/ROS
             x_train, y_train = oversampling.fit_resample(x_train, Y[train])
-            if reverse_pca:
+            if pca_mode == PCAMode.WITH_REVERSE_PCA:
                 x_train = self._pca.inverse_transform(x_train)
 
             clf.fit(x_train, y_train)
@@ -59,12 +75,9 @@ class Comparator:
     def get_acc_mean(self, acc_score):
         return round(np.mean(acc_score), 2)
 
-    def do_statystical_analysis(self, all_acc_score: List[ndarray]) -> None:
-        print("--------ALL ACC score-----------")
-        print(all_acc_score)
-
+    def do_statystical_analysis(self, all_acc_score: List[ndarray], headers: List[str]) -> None:
         # * init tables
-        shape = (4, 4)
+        shape = (len(headers), len(headers))
         t_statistic = np.zeros(shape)
         p_value = np.zeros(shape)
         advantage = np.zeros(shape)
@@ -76,7 +89,6 @@ class Comparator:
                 t_statistic[i, j], p_value[i, j] = ttest_rel(all_acc_score[i], all_acc_score[j])
 
         # * create tables for statistics
-        headers = ["SMOTE-REVERSE", "SMOTE-NO-REVERSE", "ROS-REVERSE", "ROS-NO-REVERSE"]
         names_column = np.array([[header] for header in headers])
         t_statistic_table = np.concatenate((names_column, t_statistic), axis=1)
         t_statistic_table = tabulate(t_statistic_table, headers, floatfmt=".2f")
@@ -93,18 +105,17 @@ class Comparator:
         self._logger.write(f"Statistical significance (alpha = 0.05):\n {significance_table}")
 
         stat_better = significance * advantage
-        print(stat_better.shape)
-        print(stat_better)
         stat_better_table = tabulate(np.concatenate((names_column, stat_better), axis=1), headers)
 
-
         df = pd.DataFrame(data = [{
-            'dataset': self._dataset_name,
-            headers[0]: np.array2string(np.where(stat_better[0][:] == 1)[0]),
-            headers[1]: np.array2string(np.where(stat_better[1][:] == 1)[0]),
-            headers[2]: np.array2string(np.where(stat_better[2][:] == 1)[0]),
-            headers[3]: np.array2string(np.where(stat_better[3][:] == 1)[0]),
+            headers[i]: self._stat_better_array_to_string_by_idx(i, stat_better) for i in range(shape[0])
         }])
+        df['dataset'] = '' # ! it should be blank as it will be merged
+
         self._logger.write(f"Statistically significantly better:\n {stat_better_table}")
         self._logger.write("\n----------------------------------------\n")
         return df
+
+    def _stat_better_array_to_string_by_idx(self, idx: int, stat_better: ndarray) -> str:
+        arr_str = np.array2string(np.where(stat_better[idx][:] == 1)[0], separator=', ')
+        return arr_str.replace("[","").replace("]","")
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from sklearn import tree
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 
-from comparator import Comparator
+from comparator import Comparator, PCAMode
 from loaders import ConfigLoader, KeelDatasetLoader
 from utils import Logger
 import pandas as pd
@@ -21,9 +21,17 @@ CLASSIFIRES = {
 
 OS_SMOTE = SMOTE(random_state=1000)
 OS_ROS = RandomOverSampler(sampling_strategy='auto')
+# * w/o -> without, w/ -> with, r -> reverse
+HEADERS = [
+    "SMOTE w/o PCA", "SMOTE w/ PCA", "SMOTE w/ rPCA",
+    "ROS w/o PCA", "ROS w/ PCA", "ROS w/ rPCA"
+]
 
 def merge_df(main_df: DataFrame, row_df: DataFrame) -> DataFrame:
     return pd.concat([row_df, main_df], ignore_index=True)
+
+def df_to_latex(df: DataFrame) -> str:
+    return df.style.format(precision=2).hide(axis="index").to_latex(column_format="|c|c|c|c|c|c|c|", hrules=True, siunitx=True)
 
 def main() -> None:
     dataset_loader = KeelDatasetLoader()
@@ -39,31 +47,38 @@ def main() -> None:
         for dateset_name in dataset_names:
             logger.write(dateset_name)
             dataset = dataset_loader.load(dateset_name, convert_to_ndarray=True)
+            comparator = Comparator(classifier=CLASSIFIRES[classifire_name], dataset=dataset, logger=logger)
 
-            comparator = Comparator(classifier=CLASSIFIRES[classifire_name], dataset=dataset, logger=logger, dataset_name=dateset_name)
+            # metric used: balanced_accuracy_score
+            metric_scores_for_smote_without_pca = comparator.calculate_accuracy(oversampling=OS_SMOTE, pca_mode=PCAMode.WITHOUT_PCA)
+            metric_scores_for_smote_with_pca = comparator.calculate_accuracy(oversampling=OS_SMOTE, pca_mode=PCAMode.WITH_PCA)
+            metric_scores_for_smote_with_revese_pca = comparator.calculate_accuracy(oversampling=OS_SMOTE, pca_mode=PCAMode.WITH_REVERSE_PCA)
 
-            acc_score_smote_with_reverse = comparator.calculate_accuracy(oversampling=OS_SMOTE, reverse_pca=True)
-            acc_score_smote_without_reverse = comparator.calculate_accuracy(oversampling=OS_SMOTE, reverse_pca=False)
-            acc_score_ros_with_reverse = comparator.calculate_accuracy(oversampling=OS_ROS, reverse_pca=True)
-            acc_score_ros_without_reverse = comparator.calculate_accuracy(oversampling=OS_ROS, reverse_pca=False)
+            metric_scores_for_ros_without_pca = comparator.calculate_accuracy(oversampling=OS_ROS, pca_mode=PCAMode.WITHOUT_PCA)
+            metric_scores_for_ros_with_pca = comparator.calculate_accuracy(oversampling=OS_ROS, pca_mode=PCAMode.WITH_PCA)
+            metric_scores_for_ros_with_revese_pca = comparator.calculate_accuracy(oversampling=OS_ROS, pca_mode=PCAMode.WITH_REVERSE_PCA)
 
             dataset_df_acc = pd.DataFrame(data = [{
-                'dataset': dateset_name,
-                'SMOTE-REVERSE': comparator.get_acc_mean(acc_score_smote_with_reverse),
-                'SMOTE-NO-REVERSE': comparator.get_acc_mean(acc_score_smote_without_reverse),
-                'ROS-REVERSE': comparator.get_acc_mean(acc_score_ros_with_reverse),
-                'ROS-NO-REVERSE': comparator.get_acc_mean(acc_score_ros_without_reverse)
+                'dataset': dataset.name,
+                HEADERS[0]: comparator.get_acc_mean(metric_scores_for_smote_without_pca),
+                HEADERS[1]: comparator.get_acc_mean(metric_scores_for_smote_with_pca),
+                HEADERS[2]: comparator.get_acc_mean(metric_scores_for_smote_with_revese_pca),
+                HEADERS[3]: comparator.get_acc_mean(metric_scores_for_ros_without_pca),
+                HEADERS[4]: comparator.get_acc_mean(metric_scores_for_ros_with_pca),
+                HEADERS[5]: comparator.get_acc_mean(metric_scores_for_ros_with_revese_pca),
             }])
             all_datasets_df_acc = merge_df(dataset_df_acc, all_datasets_df_acc)
 
             dataset_df_stat_signi_better = comparator.do_statystical_analysis([
-                acc_score_smote_with_reverse, acc_score_smote_without_reverse,
-                acc_score_ros_with_reverse, acc_score_ros_without_reverse
-            ])
+                metric_scores_for_smote_without_pca, metric_scores_for_smote_with_pca,
+                metric_scores_for_smote_with_revese_pca, metric_scores_for_ros_without_pca,
+                metric_scores_for_ros_with_pca,  metric_scores_for_ros_with_revese_pca
+            ], headers=HEADERS)
             all_datasets_df_stat_signi_better = merge_df(dataset_df_stat_signi_better, all_datasets_df_stat_signi_better)
 
-        all_datasets_df_acc.to_csv(f'./output/results_acc_{classifire_name}.csv')
-        all_datasets_df_stat_signi_better.to_csv(f'./output/results_better_{classifire_name}.csv')
+        output = pd.concat([all_datasets_df_acc, all_datasets_df_stat_signi_better]).sort_index(kind='merge')
+        output.to_csv(f'./output/results_all_{classifire_name}.csv')
+        logger.write(df_to_latex(output))
         all_datasets_df_acc = pd.DataFrame()
         all_datasets_df_stat_signi_better = pd.DataFrame()
 
